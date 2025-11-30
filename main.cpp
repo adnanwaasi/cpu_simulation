1d3ee745df8959d43b34ee59c7fa760f97e6a468
adnanwaasi <adnanwaasi1@gmail.com>
2025-12-01 01:24:57 +0530

    im feeling sleepy just added bunch of comments and some stuffs

--- main.cpp
+++ main.cpp
@@ -13,3 +13,24 @@ struct Process {
     int turnaround_time=0;
     bool finished =false ; 
 };
+
+void print_result( const vector<Process> &procs , const vector<pair<int,int>> gantt ){
+    /*
+        what does this function do?
+        This function prints the results of the scheduling simulation.
+
+        procs : Data structure containing all processes 
+        gantt : Gantt chart representation of process execution 
+
+        let this re-awaken your memory on Gantt charts:
+        | P1 | P2 | P3 |
+        0    3    7    10
+
+        now let's write this code 
+    */
+    cout << "processes "<< endl;
+    cout << "PID\tAT\tBT\tCT\tTAT\tWT\n";
+    double sum_tat=0.0 , sum_wt=0.0 ;
+    
+
+}
